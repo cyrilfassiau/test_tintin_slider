3359cc5f0991fa89a080ea1523588c737bcfba7e
cyrilfassiau <cyril.fassiau@gmail.com>
2025-10-14 09:48:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,36 @@ const itemData = {
             src="images/fetiche3.png",
             src="images/fetiche4.png"
         ],
-        description: "« En juin 1979, une importante exposition, destinée à voyager ensuite, sera inaugurée aux Palais des Beaux Arts de Bruxelles. Le détail de son contenu échappe à Hergé, mais ses concepteurs lui ont expliqué qu’il s’agirait de confronter les objets mythiques collectés par Tintin au cours de ses aventures aux objets réels qui les ont inspirés. Sur l’affiche de l’exposition, Hergé tente d’organiser l’incursion de ses principaux personnages au beau milieu d’une installation imaginaire propre à rendre fous de jalousie tous les collectionneurs. » Extrait : Chronologie d’une œuvre – tome 7.",
+        description: `Il est 17 h, le musée d’ethnographie ferme au son de la cloche agitée par le gardien.<br>
+Le lendemain matin, ce dernier constate le vol d’un fétiche arumbaya. Tandis que les Dupondt soupçonnent
+le personnel du musée, Tintin se lance à la recherche du voleur par une autre méthode, en approfondissant
+sa connaissance de l’objet volé. Les informations dont dispose le reporter sont minces. Le cartel du musée est
+assez succinct : un numéro d’inventaire (no 3542) et une indication de provenance, le peuple arumbaya, qui
+« habite le long du fleuve Badurayal, sur le territoire de la République du San Theodoros ». Heureusement, la
+bibliothèque de Tintin est bien fournie : il dispose des Voyages aux Amériques d’un certain Walker édité chez
+Graveau en 1875, ouvrage dans lequel une gravure du fétiche est reproduite. Le surlendemain, l’objet volé
+réapparaît miraculeusement, accompagné d’un mot du voleur expliquant qu’il a agi suite à un pari avec des
+amis. Grâce à sa documentation, Tintin acquiert la certitude que la statuette restituée n’est qu’un faux grossier,
+puisque l’oreille du fétiche est intacte…<br>
+Après d’ultimes péripéties, le (vrai) fétiche retrouvera sa place au musée en triste état, non sans avoir livré le
+secret qui attirait les malfrats : il cachait un gros diamant !<br><br>
+<strong>SOURCES :</strong><br>
+Hergé disposait, semble-t-il, d’une bonne documentation sur les Indiens Jivaros qui vivaient dans la forêt tropicale
+péruvienne. Les longues sarbacanes, les carquois et les flèches utilisées par les Arumbayas sont les copies conformes
+de ceux des Jivaros ou Jibaros. Le nom de ces Indiens réducteurs de tête sera à l’origine du nom des ennemis jurés des
+Arumbayas, les Bibaros. 
+
+<br><br>
+
+Pour ce qui concerne l’objet au centre de l’histoire, Hergé n’a pas dû aller bien loin pour le trouver : les musées royaux
+d’Art et d’Histoire de Bruxelles possèdent encore aujourd’hui une statuette en bois, identique au fétiche (dont le pied
+est cassé, et non l’oreille...). Il s’agit d’une figurine précolombienne originaire du Pérou.
+
+<br><br>
+
+Mesurant cinquante-cinq centimètres de haut, cette représentation masculine d’un « porteur d’offrande » est issue de
+la culture chimú, population établie sur la côte septentrionale du Pérou du XIIème au XVème siècle de notre ère ayant
+comme capitale Chanchan. Les Chimus seront soumis par les Incas vers 1460.`,
        
     },
     sceptre: {
@@ -18,7 +47,21 @@ const itemData = {
             src="images/sceptre3.png",
             src="images/sceptre4.png"
         ],
-        description: "Le sceptre et pas n’importe quel sceptre: Le Sceptre d’Ottokar. La collection Les Icônes Cette fois, c’est le symbole royal qui est mis à l’honneur: Le symbole royal de la Syldavie, minuscule pays constitué de quelques villages et de forêts, si petit que Tintin n'a pas de difficulté à le traverser rapidement d’un bout à l’autre. ",
+        description: `Petit royaume attaché à ses traditions, la Syldavie possède aussi son emblème royal : le sceptre d’Ottokar, du
+nom du roi Ottokar IV, monté sur le trône en 1360. Le sceptre a sauvé la vie d’Ottokar quand celui-ci l’a utilisé
+comme arme pour assommer le baron félon qui convoitait son trône.<br>
+C’est alors qu’Ottokar IV prononça ces mots devenus historiques :<br><br>
+« Ô sceptre ! Tu m’as sauvé la vie. Sois donc désormais le signe suprême de la royauté syldave.
+Malheur au roi qui te perdra, car, je le proclame, celui-là ne serait plus digne de régner. »<br><br>
+Arrivé en Syldavie, Tintin comprend qu’un complot est organisé pour destituer le roi Muskar XII.
+Par quel moyen ? En lui dérobant son sceptre, un long bâton doré surmonté d’un pélican aux ailes déployées.
+Si le souverain s’affiche sans lui lors des festivités de la fête nationale, la Saint- Wladimir, il devra abdiquer,
+ouvrant la porte au chaos et aux ambitions d’une puissance étrangère, la Bordurie.<br><br>
+<strong>LE SAVAIS-TU ?</strong><br>
+Le sceptre d’Ottokar existe, et Hergé ne le savait pas. En 1976, des travaux de restauration entrepris dans la
+Cathédrale Saint Vitus, au Château de Prague, ont permis la mise à jour des attributs royaux d’Ottokar II, roi de
+Bohème (1230-1278) et membre de la dynastie des Premyslides. Le sceptre se trouvait parmi ces attributs: un
+trésor vieux de cinq siècles..`,
        
     },
     mochica: {
@@ -28,7 +71,23 @@ const itemData = {
             src="images/mochica1.png",
             src="images/mochica2.png"
         ],
-        description: "Comme ce fut le cas pour la création de plusieurs de ses aventures, Hergé se rendit au musée du Cinquantenaire qui fait partie des Musées royaux d'Art et d'Histoire, afin de se documenter. C'est notamment en ces lieux qu'il vit un modèle de vase-portrait (communément appelé vase étrier car grâce à sa boucle, il était possible d'y passer une corde et d'ainsi l'attacher aux lamas avec d'autres vases du même type) de la culture Mochica dont il s'inspira pour dessiner celui présent à la page 45 de l'aventure de Tintin.",
+        description: `<strong>LE MYSTÉRIEUX BRIC-À-BRAC DU BROCANTEUR</strong><br>
+Pour se lancer à la découverte du trésor de Rackham le Rouge, Tintin et la capitaine Haddock sont à la recherche
+d’un scaphandre. Un équipement complet est à vendre à l’état neuf chez un brocanteur. Cette boutique de marine
+regorge d’objets probablement repérés par Hergé dans les riches collections des Musées royaux d’art et d’histoire
+du Cinquantenaire, à Bruxelles.<br>
+Le vase-portrait qu’Hergé a dessiné est bien connu : il est reproduit dans plusieurs ouvrages, et provient de
+Chimbote. C’est une véritable pièce de musée, découverte par A.Baessler à la fin du XIXème siècle. On retrouve
+sa photo dans la documentation d’Hergé.<br><br>
+<strong>LE SAVAIS-TU ?</strong><br>
+La céramique mochica, ou moche, recouvre les créations des populations précolombiennes installées tout au long
+de la côte nord-péruvienne, dans les oasis côtières et sur les contreforts de la chaîne andine, de 100 av. J.-C. à 700.<br>
+La culture mochica, contemporaine de la culture nazca sud-péruvienne, est la seule civilisation précolombienne
+à avoir créé en céramique des scènes complexes avec interaction de personnages multiples, notamment dans sa
+poterie funéraire, qui était utilisée en grande quantité pour accompagner les défunts dans leur sépulture.
+Moyen d’expression privilégié d’un peuple qui ne connaissait pas l’écriture, la céramique mochica revêt une impor-
+tance fondamentale pour la connaissance de la culture moche ou mochica. Elle en constitue une encyclopédie
+visuelle sans équivalent dans aucune autre culture de l’Amérique ancienne.`,
        
     },
     civa: {
@@ -38,7 +97,33 @@ const itemData = {
             src="images/civa2.png",
             src="images/civa3.png"
         ],
-        description: "Qui est Shiva ? Il est généralement représenté comme un être soit plongé dans une méditation profonde soit, comme on le voit dans Les Cigares du Pharaon (p.50), dansant le Nataraja . Shiva signifie en sanskrit ‘ le bon, celui qui porte bonheur ‘. Dans la pratique hindouiste, Shiva qui appartient au pan - théon védique est considéré différemment selon les tradi - tions: selon la tradition Shaiva, Shiva est considéré comme le Dieu suprême, et selon la tradition Smarta il est perçu comme l’une des cinq formes primaires de Dieu.",
+        description: `Tout jeune, Hergé a découvert les principaux jalons de l’histoire de l’art.
+Certains d’entre eux n’attendaient que l’occasion de ressurgir.<br>
+C’est sous le pseudonyme d’Hergé, dont il s’était doté en 1924, que le collégien Georges Remi collabore régu-
+lièrement au mensuel Le Boy-Scout. Les illustrations qu’il y publie attestent son souci de qualité. C’est pour
+le soutenir dans cette voie prometteuse que le mensuel scout lui a offert L’Anthologie d’art d’Alfred Lenoir,
+publiée chez Armand Colin, à Paris, un ouvrage superbement relié, dédicacé en ces termes :
+« La revue Le Boy-Scout en hommage de reconnaissance à son collaborateur Georges Remi, en la fête de saint
+Georges, avril 1925, René Weverbergh, directeur ».
+Nul doute que ce beau livre aura effectivement contribué à l’ouverture artistique du jeune homme. Huit ans
+plus tard, dans le tombeau de Kih-Oskh (Les Cigares du Pharaon), Tintin passera devant une fresque nette-
+ment inspirée d’un bas-relief représentant Séthi Ier et la déesse Hathor conservé au Louvre... et tiré de ladite
+Anthologie d’art. D’autres œuvres d’art apparaissent dans Les Cigares du Pharaon, comme le dossier du trône
+de Tout-Ankh-Amon, qui donnera naissance à une vision cauchemardesque de Tintin, ou, pour ce qui concerne
+l’art indien, la statue de Çiva dansant ou encore celle de Vichnou. Dans Les Cigares du Pharaon, le roi de la
+danse, Nataraja, est convoqué par Hergé pour son aspect destructeur, mais il refuse le sacrifice de Milou qui lui
+est offert.<br><br>
+<strong>LE SAVAIS-TU ?</strong><br>
+SHIVA NATARAJA - Shiva signifie en sanskrit « le bon, celui qui porte bonheur ».
+Dieu fécondateur et destructeur, troisième personne de la Trinité hindoue avec Vishnou et Brahma,
+Shiva est ici représenté en seigneur de la danse.<br>
+Le dieu Shiva fait partie, avec Brahma et Visnu, de la triade des dieux majeurs de l’hindouisme. Shiva signifie en sanskrit le
+« gracieux ». Il est le dieu de la destruction porteuse de création. Il est ici représenté comme « roi (raja) de la Danse (nata) »
+par le mouvement qu’il esquisse, il incarne l’énergie cosmique d’où émane un cycle de création et de destruction en perpé-
+tuelle réciprocité. La statue le montre entouré d’un cercle de flammes qui représente cette énergie cosmique. Les attitudes
+des membres symbolisent les cinq activités de Shiva : la création, la conservation, la destruction, l’énergie et la rédemption.<br>
+
+`,
        
     },
     sirius: {
@@ -48,7 +133,24 @@ const itemData = {
             src="images/sirius2.png",
             src="images/sirius3.png"
         ],
-        description: "Le Sirius est un chalutier commandé par le capitaine Chester. Il le prêta à son ami le capitaine Archibald Haddock afin que ce dernier puisse partir à la recherche du trésor de Rackham le Rouge dans l'épave de La Licorne. ",
+        description: `Cap sur l’archipel des Antilles où gît l’épave de la Licorne !<br>
+Tintin et le capitaine Haddock ont embarqué à bord du Sirius, le chalutier loué par Haddock à son ami
+le capitaine Chester. Ce robuste et très fiable bateau de pêche a d’abord vogué dans les eaux profondes de
+L’Étoile Mystérieuse avant de s’aventurer dans celles du Trésor de Rackham le Rouge.<br><br>
+A peine ont-ils quitté le port qu’ils sont rejoints par les Dupondt, dépêchés pour les protéger d’un possible
+mauvais coup de l’antiquaire indélicat Maxime Loiseau.<br><br>
+Ils découvrent un passager clandestin : le professeur Tournesol qui s’est invité à bord avec sa dernière
+invention, un mini-sous-marin en forme de requin. A la lecture du numéro de décembre 1936 de la revue
+Wandelaer-sur l’eau, Hergé a l’attention attirée par la photo et l’article se rapportant à un chalutier qui vient
+d‘être mis à l’eau à Ostende : le John - O 88.<br><br>
+Long de 39 mètres, ce chalutier aux lignes modernes évoquant celles d’un petit paquebot a été construit, sur
+des plans de l’ingénieur Childsky, par les chantiers Boel & fils à Tamise pour le compte de l’armement Brunet,
+d’Ostende. Au début 1943, quand il entreprend de dessiner Le Trésor de Rackham le Rouge, Hergé décide de
+faire du John - O 88 le modèle du Sirius.<br><br>
+Le collectionneur Alexandre Berqueman lui procure les plans et la maquette du chalutier ostendais.<br><br>
+<strong>LE SAVAIS-TU ?</strong><br>
+Le nom du Sirius est emprunté au premier navire à avoir réussi la traversée de l’Atlantique à la vapeur.
+En 1838, ce célèbre steamer anglais avait accompli cet exploit en 18 jours.`,
         
     },
     fusee: {
@@ -58,7 +160,36 @@ const itemData = {
             src="images/fusee1.png",
             src="images/fusee2.png"
         ],
-        description: "Cette fusée rouge et blanche devient une figure à part entière. Aucun véhicule réel ne lui ressemble, et pourtant, elle est immédiatement reconnaissable. Sa silhouette élancée, ses damiers géométriques, ses ailerons courbes en font un objet graphique parfait, un totem de science maîtrisée. Elle dépasse le cadre de l’album pour devenir un emblème universel, repris sur des affiches, transformé en jouet, décliné sous toutes les formes. On la retrouve dans les vitrines comme dans les musées, dans les intérieurs design comme dans les chambres d’enfants. ",
+        description: `Sur le pas de tir, après les dernières vérifications d’usage, la fusée décolle du Centre de Recherches Atomiques
+de Sbrodj… direction la Lune ! L’engin à propulsion nucléaire, secrètement mis au point par le Mammouth
+– alias Tryphon Tournesol – s’élance enfin dans un épais panache de fumée, embarquant à son bord Tintin,
+Milou, Haddock, les Dupondt, Franck Wolf et bien entendu, le professeur, pour le tout premier vol spatial
+habité de l’Histoire.<br>
+Tintin et ses amis sont des précurseurs. Anticipant de quinze ans la première mission lunaire américaine,
+avant l’Homme, ils sont allés dans l’espace. Avant l’Homme, ils ont marché sur la Lune. Ce double exploit qui a
+devancé l’Histoire est à jamais associé à l’engin avant-gardiste: l’extraordinaire fusée du professeur Tournesol.
+Grâce à son design bicolore profilé, son damier rouge et blanc, elle s’est rapidement imposée comme un sym-
+bole fort de l’œuvre d’Hergé devenant un des objets culte de la bande dessinée.<br>
+Féru de sciences, Hergé a empreint ses récits d’hyperréalisme. Février 1949, alors qu’Hergé se documente sans
+cesse, une fusée à étages est lancée, avec succès, de la base américaine de White Sands. Parmi les sources
+d’Hergé, L’Astronautique du professeur Alexandre Ananoff, président de la Société d’Aéronautique française,
+sera sa référence essentielle. Hergé s’inspire de chaque chapitre tant pour concevoir la forme de la fusée que
+celle des couchettes, en passant par le poste de pilotage, ou encore le type de carburant utilisé. Deux autres
+spécialistes émergent de sa documentation : Bernard Heuvelmans, journaliste, grand vulgarisateur scienti-
+fique et auteur de L’Homme parmi les étoiles, sans oublier le professeur suisse Auguste Piccard, modèle de
+Tournesol, explorateur de la stratosphère comme des profondeurs marines. Hergé s’inspirera aussi, pour le char
+lunaire et pour les scaphandres, des travaux de l’illustrateur américain Chesley Bonestell, qui travaillait pour le
+tournage d’un film à Hollywood, Destination Moon (1950).<br>
+Animé par son extrême souci d’exactitude, Hergé fera également fabriquer une maquette des trois étages
+supérieurs de la fusée par Arthur Vannoeyen. Elle est démontable afin que chaque collaborateur des Studios
+Hergé puisse en dessiner des éléments et y faire évoluer les différents personnages. Elle est aujourd’hui expo-
+sée au Musée Hergé, à Louvain-la-Neuve non loin de la maquette du char lunaire, du même auteur.<br><br>
+<strong>LE SAVAIS-TU ?</strong><br>
+C’est à cette époque qu’Hergé décide de s’entourer de collaborateurs. Il crée les Studios Hergé (1950). Bob De
+Moor, dessinateur brillant, a largement contribué aux décors des deux albums « Objectif Lune et On a maché
+sur la Lune. Il restera 35 ans à ses côtés.
+En mars 1952, paraît On a marché sur la Lune dans le journal Tintin
+Le 21 juillet 1969, Neil Armstrong pose le premier pied sur la Lune.`,
        
     }
 };
@@ -75,7 +206,7 @@ function showDetails(itemKey) {
 
     document.getElementById('detailsTitle').textContent = item.title;
     document.getElementById('detailsCategory').textContent = item.category;
-    document.getElementById('detailsDescription').textContent = item.description;
+    document.getElementById('detailsDescription').innerHTML = item.description;
 
 
     updateGalleryImage();
